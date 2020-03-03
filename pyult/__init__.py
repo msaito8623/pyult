dd5163bd0d399523618f48469e27a7785b80f77b
motoki <motoki@saito.sfs.uni-tuebingen.de>
2020-03-03 16:27:48 +0100

    Minor modification

--- pyult/__init__.py
+++ pyult/__init__.py
@@ -6,21 +6,5 @@ Ultrasound images exported by an ultrasound-recording software AAA can be prepro
 
 from pyult.pyult import *
 
-__author__ = 'Motoki Saito'
-__author_email__ = 'motoki.saito@uni-tuebingen.de'
 __version__ = '0.0.1'
-__license__ = '*To be updated*'
-__description__ = 'Preprocessing ultrasound images from AAA in Python.' 
-__classifiers__ = [
-    'Development Status :: 3 - Alpha',
-    'Environment :: Console',
-    'Intended Audience :: Science/Research',
-    'License :: OSI Approved :: MIT License',
-    'Operating System :: POSIX :: Linux',
-    'Operating System :: MacOS',
-    'Programming Language :: Python',
-    'Programming Language :: Python :: 3.7',
-    'Programming Language :: Python :: 3 :: Only',
-    'Topic :: Scientific/Engineering',
-    ]
 
