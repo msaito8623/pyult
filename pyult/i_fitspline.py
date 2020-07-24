e9d58886cb008631e435aac4e27973ba75aa1fa6
motoki <motoki@saito.sfs.uni-tuebingen.de>
2020-07-24 20:51:09 +0900

    Fixed interactive functions, so i_fitspline can produce fitted values, and i_makefan has a new argument "magnify".

--- pyult/i_fitspline.py
+++ pyult/i_fitspline.py
@@ -1,18 +1,32 @@
 import argparse
 parser = argparse.ArgumentParser(description='Produces an image with a fitted spline curve from an ultrasound image.')
 parser.add_argument('path', help='Path to the image file you want to add a spline curve.')
+parser.add_argument('--cores', type=int, help='How many cores to use.')
+parser.add_argument('--value', action='store_true', help='Returns fitted y-axis values of a fitted spline curve if specified.')
 args = parser.parse_args()
 
+import pandas as pd
 import pyult.pyult as pyult
 import pyult.splfit as splfit
 from pathlib import Path
 
 upc = pyult.UltPicture()
 img = upc.read_img(args.path)
-img = splfit.fit_spline_img(img)
+
+
+if args.value:
+    val = splfit.fit_spline(img, cores=args.cores)
+    val = pd.DataFrame(val)
+else:
+    img = splfit.fit_spline_img(img, cores=args.cores)
+
+
 pdir = str(Path(args.path).parent)
 fname = str(Path(args.path).stem)
-sfx = str(Path(args.path).suffix)
+if args.value:
+    sfx = '.csv'
+else:
+    sfx = str(Path(args.path).suffix)
 opath = '{}/{}_spline{}'.format(pdir, fname, sfx)
 
 flg = True
@@ -23,4 +37,8 @@ while flg:
         cnt = cnt + 1
     else:
         flg = False
-upc.save_img(path=opath, img=img)
+
+if args.value:
+    val.to_csv(opath, sep='\t', index=False, header=True)
+else:
+    upc.save_img(path=opath, img=img)
